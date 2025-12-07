f1fc1c74d11ec5c66c8190abcf052e65fc88b61a
bhoomikabanakar3 <bhoomikabanakar3@gmail.com>
2025-12-07 13:06:28 +0530

    Add files via upload

---
+++ charpractice.java
@@ -0,0 +1,18 @@
+package placementprograms;
+
+public class charpractice {
+
+	public static void main(String[] args) {
+		char a= 'B';
+		char b= 'C';
+		System.out.println("before swapping" +a);
+		System.out.println("before swapping" +b);
+		char c= a;
+		a=b;
+		b=c;
+		System.out.println("after swapping" +a);
+		System.out.println("after swapping" +b);
+
+	}
+
+}
